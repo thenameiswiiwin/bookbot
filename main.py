5d4b092a76c2e710d9d5fc34bbd5707986b494dc
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-04-22 21:31:18 -0400

    feat: characters count

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def main():
     path = "./books/frankenstein.txt"
     text = get_book_text(path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    num_chars = get_num_chars(text)
+    print(num_chars)
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_num_words(text):
     return len(text.split())
+
+def get_num_chars(text):
+    chars = {}
+    for char in text.lower():
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
