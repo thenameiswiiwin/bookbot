94cbca3df8b7183f76711a14f972c93e1172591e
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-04-23 00:14:48 -0400

    feat: sys module, access command line

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from stats import get_num_words, get_num_chars, sorted_list
+import sys
+from stats import (
+        get_num_words, 
+        get_num_chars,
+        sorted_list,
+)
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
     sorted = sorted_list(num_chars)
+    print_report(path, num_words, sorted)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def print_report(path, num_words, sorted):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}")
     print("----------- Word Count ----------")
@@ -17,8 +32,5 @@ def main():
         print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
 
 main()
