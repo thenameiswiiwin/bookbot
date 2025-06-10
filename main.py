ee548861749b797591971075347cd7f820fb3223
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-06-10 11:44:56 -0400

    refactor: move word count function to stats.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_num_words
+
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
@@ -7,9 +9,5 @@ def main():
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 main()
