afdf99cf7ac8baf923eabe5dd5768be0e6d008be
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-06-10 11:51:24 -0400

    feat: Implement character counting and display stats

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import get_num_words
+from stats import get_num_words, get_chars_dict
 
 def main():
     path = "books/frankenstein.txt"
     text = get_book_text(path)
     num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
     print(f"{num_words} words found in the document")
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
