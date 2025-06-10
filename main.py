c5ca424b65443514dd2c038e70e0d2291589155a
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-06-10 12:06:32 -0400

    feat: Add command line argument for book path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words,
     get_chars_dict,
@@ -5,7 +6,10 @@ from stats import (
 )
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
     text = get_book_text(path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
